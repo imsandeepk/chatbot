b6eda019b7e21987c5eace4284d2924fc6574e40
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-05-31 16:40:14 +0100

    lumo added

--- index.py
+++ index.py
@@ -19,20 +19,41 @@ def process():
     chat_completion = prompt(input)
     return render_template('index.html', output=chat_completion)
 
+
+def check(chat_completion_lumo):
+    output = ["0","1","2"]
+    if chat_completion_lumo.choices[0].text in output:
+        if chat_completion_lumo.choices[0].text=="0":
+            return "Lumo-Low"
+        if chat_completion_lumo.choices[0].text=="1":
+            return "Lumo-Medium"
+        if chat_completion_lumo.choices[0].text=="2":
+            return "Lumo-High"
+        else:
+            return "Lumo-None"
+    
 def prompt(input):
     chat_completion = openai.Completion.create(model="ada:ft-birmingham-digital-chemistry-2023-03-06-07-46-06", prompt= input, temperature=0, max_tokens=1)
+    chat_completion_lumo = openai.Completion.create(model="ada:ft-birmingham-digital-chemistry-2023-03-06-15-11-11", prompt= input, temperature=0, max_tokens=1)
     output = ["0","1","2"]
-    print(chat_completion.choices[0].text)
-    print(type(chat_completion.choices[0].text))
+    print(chat_completion_lumo.choices[0].text)
     if chat_completion.choices[0].text in output:
         if chat_completion.choices[0].text=="0":
-            return "Low"
+            return "Homo-Low",check(chat_completion_lumo)
         if chat_completion.choices[0].text=="1":
-            return "Medium"
+            check(chat_completion_lumo)
+            return "Homo-Medium",check(chat_completion_lumo)
         if chat_completion.choices[0].text=="2":
-            return "High"
-    else:
-        return "None";
+            check(chat_completion_lumo)
+            return "Homo-High",check(chat_completion_lumo)
+        else:
+            return "Homo-None",check(chat_completion_lumo)
+        
+    
+    
+        
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
